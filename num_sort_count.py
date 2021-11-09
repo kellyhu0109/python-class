3fc2f19eeeb31ec8793e056163e93ca79d68f254
kellyhu0109 <kellyhu0109@gmail.com>
2021-11-09 11:03:47 +0800

    renew num_sort_count.py multi-line input

--- num_sort_count.py
+++ num_sort_count.py
@@ -1,16 +1,26 @@
-# q = '3337777899922222222222222222'
-q = '1112223334444555566667777'
-cnt = 1
-cnt_list = []
+# import sys
+# q = sys.stdin.read()
 
-for i in range(1, len(q)):
-    if int(q[i-1]) <= int(q[i]):
-        cnt += 1
-    else:
-        cnt_list.append(cnt)
-        cnt = 1
+q = '''1112223334444555566667777
+3337777899922222222222222222'''
 
-    if i == len(q)-1:
-        cnt_list.append(cnt)
+q_list = q.splitlines()
+output = []
 
-print(max(cnt_list))
+for n in range(len(q_list)):
+    cnt = 1
+    cnt_list = []
+    for i in range(1, len(q_list[n])):
+        if int(q_list[n][i-1]) <= int(q_list[n][i]):
+            cnt += 1
+        else:
+            cnt_list.append(cnt)
+            cnt = 1
+
+        if i == len(q_list[n])-1:
+            cnt_list.append(cnt)
+
+    output.append(max(cnt_list))
+
+for x in output:
+    print(x)
